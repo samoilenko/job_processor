dd55a460255b75d9de99d323b1a5ae277b329700
Serhii Samoilenko <s.samoilenko@gmail.com>
2025-06-10 20:27:44 +0300

    add random time execution to the dummy cpp job

--- packages/jobProcessor/infrastructure/job_cpp.sh
+++ packages/jobProcessor/infrastructure/job_cpp.sh
@@ -4,9 +4,11 @@ echo "Running simulated C++ job..."
 sleep 1
 
 if ((RANDOM % 2 == 0)); then
+    sleep "0.0$((30 + RANDOM % 41))"
     echo "Job succeeded."
     exit 0
 else
+    sleep "0.0$((30 + RANDOM % 41))"
     echo "Job failed."
     exit 1
 fi
