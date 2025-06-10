2fcef543784ca3e8a48e9702f45660bd843a79fa
Serhii Samoilenko <s.samoilenko@gmail.com>
2025-06-10 20:28:32 +0300

    add average time worker

---
+++ packages/statistic/infrastructure/workers/AverageTimeExecution.ts
@@ -0,0 +1,110 @@
+import { IStatisticLogger } from "../../domain/statisticTypes";
+import JobService from "../../../job/domain/Service";
+import Inbox from "../Inbox";
+import Semaphore from "../../../utils/Semaphore"
+import Outbox from "../Outbox";
+
+const calculateAverageTimeExecution = (timeExecution: number, count: number): number => {
+    return Math.round(timeExecution / count);
+}
+
+export default class AverageTimeExecution {
+    #logger: IStatisticLogger
+    #jobService: JobService
+    #inbox: Inbox
+    #outbox: Outbox;
+    #semaphore: Semaphore
+
+    #totalJobs: number = 0;
+    #succeed: number = 0;
+    #failed: number = 0;
+    #failedTotalExecutionTime = 0;
+    #successTotalExecutionTime = 0;
+
+    constructor(params: { logger: IStatisticLogger, jobService: JobService, inbox: Inbox, outbox: Outbox }) {
+        this.#logger = params.logger;
+        this.#jobService = params.jobService;
+        this.#inbox = params.inbox;
+        this.#outbox = params.outbox;
+        this.#semaphore = new Semaphore();
+    }
+
+    async run() {
+        for await (const event of this.#inbox.getEvent()) {
+            await this.#semaphore.take();
+            this.#handle(event)
+                .catch((e) => {
+                    this.#logger.error(e, event);
+                })
+                .finally(() => this.#semaphore.return());
+        }
+    }
+
+    async #handle(event: { type: string, payload: Record<string, unknown> }) {
+        if (event.type === 'jobRegistered') {
+            const job = await this.#getJob(event.payload.id as string);
+            if (!job) {
+                this.#logger.error(`Job not found`, event);
+                return;
+            }
+            this.#totalJobs++
+        }
+
+        if (event.type === 'jobSuccessed') {
+            const job = await this.#getJob(event.payload.id as string);
+            if (!job) {
+                this.#logger.error(`Job not found`, event);
+                return;
+            }
+
+            const executionTime: number | undefined = event.payload.executionTime as number | undefined;
+            if (executionTime === undefined) {
+                this.#logger.error(`Event doesn't have execution time`, event);
+                return;
+            }
+
+            this.#successTotalExecutionTime += executionTime
+            this.#succeed++
+        }
+
+        if (event.type === 'jobFailed') {
+            const job = await this.#getJob(event.payload.id as string);
+            if (!job) {
+                this.#logger.error(`Job not found`, event);
+                return;
+            }
+            const executionTime: number | undefined = event.payload.executionTime as number | undefined;
+            if (executionTime === undefined) {
+                this.#logger.error(`Event doesn't have execution time`, event);
+                return;
+            }
+
+            this.#failedTotalExecutionTime += executionTime
+            this.#failed++
+        }
+
+        const successAverageTime = calculateAverageTimeExecution(this.#successTotalExecutionTime, this.#succeed);
+        const failedAverageTime = calculateAverageTimeExecution(this.#failedTotalExecutionTime, this.#succeed);
+        this.#outbox.add('statisticCalculated', {
+            id: this.constructor.name,
+            pattern: 'Average time execution',
+            successJobs: `${this.#succeed}/${this.#totalJobs} ${successAverageTime}ms`,
+            failedJobs: `${this.#failed}/${this.#totalJobs} ${failedAverageTime}ms`,
+        });
+    }
+
+    async #getJob(jobId: string) {
+        if (!jobId) {
+            this.#logger.error('Job id is empty');
+            return;
+        }
+
+        const job = this.#jobService.get(jobId);
+        if (!job) {
+            this.#logger.error(`Job '${jobId} not found'`);
+            return;
+        }
+
+        return job;
+    }
+}
