d209bdf683b7a25bada3bb10089cd1d030f67b27
Serhii Samoilenko <s.samoilenko@gmail.com>
2025-06-12 11:40:00 +0300

    expnad logger with new methods

--- src/ConsoleLogger.ts
+++ src/ConsoleLogger.ts
@@ -2,4 +2,12 @@ export default class ConsoleLogger {
     error(...args: unknown[]) {
         console.error(...args);
     }
+
+    info(...args: unknown[]) {
+        console.info(...args);
+    }
+
+    debug(...args: unknown[]) {
+        console.debug(...args);
+    }
 }
